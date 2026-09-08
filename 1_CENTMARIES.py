ad0c0e147e86e5dd32343ea4c6d4993d46c6d8f5
tanush-devs <tanushm22@gmail.com>
2026-09-08 21:56:45 +0530

    Centmaries_question_v2

--- 1_CENTMARIES.py
+++ 1_CENTMARIES.py
@@ -1,23 +1,38 @@
 def factorail(num):
     if num == 1 or num == 0:
         return 1
-    
+
     return num * factorail (num - 1)
 
-def next_occurence(x,occurence_num):
-    if occurence_num == 1:
-        return x
-    else:
-        if occurence_num % 2 == 0:
-            return (x**occurence_num) / factorail(occurence_num)
-        elif occurence_num % 2 != 0:
-            return -((x**occurence_num) / factorail(occurence_num))
+def find_summation(x, total_terms):
+    summation = 0
+    for i in range(total_terms,0, -1):
+        if i == 1:
+            return summation + x
+        elif i % 2 == 0:
+            summation += (x**i)/factorail(i)
+        elif i % 2 != 0:
+            summation += -(x**i)/factorail(i)
 
 x = int(input("Choose a value for X: "))
-n = int(input("Till what range do you wanna find the summision: "))
+n = int(input("Till what range do you wanna find the summation: "))
 
-answer = 1
-for i in range(1,n+1):
-    answer *= next_occurence(x, i)
+answer = find_summation(x, n)
 
 print(f"Summision of {n} terms of the sequence is {answer}")
+
+
+## without functions/school level
+
+summation = 0
+for i in range(1,n+1):
+    if i == 1:
+        summation += x
+    elif i%2 == 0:
+        summation += (x**i)/factorail(i)
+    elif i%2 != 0:
+        summation += -(x**i)/factorail(i)
+    else:
+        pass
+    
+print(f"Summation of {n} terms of the sequence is {summation}")
